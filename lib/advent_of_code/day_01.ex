1e9ae7593fa502175956ad50938d95010dd34521
Jarem Andersen <50599657+JaremAndersen@users.noreply.github.com>
2022-11-30 22:57:45 -0700

    EZ day one

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,25 @@
 defmodule AdventOfCode.Day01 do
   def part1(_args) do
+    AdventOfCode.Input.get!(1,2022)
+    |> String.split("\n\n")
+    |> Enum.map( fn x ->
+      String.split(x,"\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
+    |> Enum.max()
   end
 
   def part2(_args) do
+    AdventOfCode.Input.get!(1,2022)
+    |> String.split("\n\n")
+    |> Enum.map( fn x ->
+      String.split(x,"\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
+    |> Enum.sort()
+    |> Enum.take(-3)
+    |> Enum.sum
   end
 end
