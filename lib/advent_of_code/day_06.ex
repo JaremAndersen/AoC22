799d6a93414c235b09504e10ad57f79e62d262a8
Jarem Andersen <50599657+JaremAndersen@users.noreply.github.com>
2022-12-05 22:28:36 -0700

    day 6

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,17 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(input) do
+    input |> String.graphemes() |> find_start(0, 4)
   end
 
-  def part2(_args) do
+  def find_start(enum, start, target_size) do
+    if Enum.slice(enum, start, target_size) |> MapSet.new() |> MapSet.size() < target_size do
+      find_start(enum, start + 1, target_size)
+    else
+      start + target_size
+    end
+  end
+
+  def part2(input) do
+    input |> String.graphemes() |> find_start(0, 14)
   end
 end
