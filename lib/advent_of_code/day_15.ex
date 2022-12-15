90d122300f27cec28983aab85a6cceda3502731d
Jarem Andersen <jarem.andersen@pdq.com>
2022-12-15 12:38:22 -0700

    day 15
    boy this is messy

--- lib/advent_of_code/day_15.ex
+++ lib/advent_of_code/day_15.ex
@@ -1,7 +1,123 @@
 defmodule AdventOfCode.Day15 do
-  def part1(_args) do
+  def part1(input) do
+    beacons_sensors =
+      Enum.reduce(input, %{}, fn {sensor, beacon}, sb ->
+        Map.merge(sb, %{sensor => :sensor, beacon => :beacon})
+      end)
+
+    sensor_distances =
+      Enum.reduce(input, %{}, fn {{sx, sy}, beacon}, distances ->
+        dist = calc_distance({sx, sy}, beacon)
+        Map.put(distances, {sx, sy}, dist)
+      end)
+
+    check_row(sensor_distances, beacons_sensors, 2_000_000)
   end
 
-  def part2(_args) do
+  def check_row(sensor_distances, bs, row) do
+    in_row =
+      Map.keys(sensor_distances)
+      |> Enum.map(fn {x, _y} -> x end)
+
+    min = Enum.min(in_row) - 1_100_000
+    max = Enum.max(in_row) + 1_100_000
+
+    Enum.reduce(min..max, 0, fn x, acc ->
+      near_sensor =
+        Enum.any?(sensor_distances, fn {{sx, sy}, s_dist} ->
+          dist = calc_distance({sx, sy}, {x, row})
+
+          !Map.has_key?(bs, {x, row}) && dist <= s_dist
+        end)
+
+      if near_sensor do
+        acc + 1
+      else
+        acc
+      end
+    end)
+  end
+
+  def print(no_beacons) do
+    IO.inspect(no_beacons)
+
+    Enum.reduce(0..20, "", fn y, output ->
+      row =
+        Enum.map(0..20, fn x ->
+          case Map.get(no_beacons, {x, y}) do
+            :beacon -> "B"
+            :sensor -> "S"
+            :empty -> "#"
+            :perim -> "#"
+            _ -> "."
+          end
+        end)
+        |> Enum.join("")
+
+      output <> "#{y}" <> row <> "\n"
+    end)
+    |> IO.puts()
+  end
+
+  def calc_distance({sx, sy}, {bx, by}) do
+    abs(sx - bx) + abs(sy - by)
+  end
+
+  def part2(input) do
+    sensor_distances =
+      Enum.reduce(input, %{}, fn {{sx, sy}, beacon}, distances ->
+        dist = calc_distance({sx, sy}, beacon)
+        Map.put(distances, {sx, sy}, dist)
+      end)
+      |> IO.inspect()
+
+    perims =
+      Enum.reduce(sensor_distances, %{}, fn {sensor, distance}, perims ->
+        IO.inspect(sensor, label: "sensor")
+        new_perims = find_perimeters({sensor, distance})
+        sensors = %{sensor => :sensor}
+        merged = Map.merge(new_perims, sensors)
+        Map.merge(perims, merged)
+      end)
+      |> IO.inspect()
+
+    {x, y} = check_perimeters(perims, sensor_distances)
+    x * 4_000_000 + y
+  end
+
+  @lower_bound 0
+  @upper_bound 4_000_000
+
+  def check_perimeters(perims, sensors) do
+    Enum.reduce_while(perims, nil, fn {{px, py}, _}, found ->
+      near_sensor =
+        Enum.any?(sensors, fn {{sx, sy}, s_dist} ->
+          dist = calc_distance({sx, sy}, {px, py})
+
+          dist <= s_dist or not in_bound({px, py})
+        end)
+
+      if near_sensor do
+        {:cont, found}
+      else
+        {:halt, {px, py}}
+      end
+    end)
+  end
+
+  def in_bound({x, y}) do
+    x >= @lower_bound and
+      x <= @upper_bound and
+      y >= @lower_bound and
+      y <= @upper_bound
+  end
+
+  def find_perimeters({{sx, sy}, dist}) do
+    Enum.reduce((sy - dist - 1)..(sy + dist + 1), %{}, fn y, perims ->
+      x_delta = abs(abs(y - sy) - dist - 1)
+
+      search_perims = %{{sx - x_delta, y} => :perim, {sx + x_delta, y} => :perim}
+      Map.merge(perims, search_perims)
+    end)
   end
 end
--- lib/mix/tasks/d15.p2.ex
+++ lib/mix/tasks/d15.p2.ex
@@ -5,7 +5,45 @@ defmodule Mix.Tasks.D15.P2 do
 
   @shortdoc "Day 15 Part 2"
   def run(args) do
-    input = nil
+    # "Sensor at x=8, y=7: closest beacon is at x=2, y=10\n"
+    input =
+      AdventOfCode.Input.get!(15, 2022)
+      # "Sensor at x=2, y=18: closest beacon is at x=-2, y=15\nSensor at x=9, y=16: closest beacon is at x=10, y=16\nSensor at x=13, y=2: closest beacon is at x=15, y=3\nSensor at x=12, y=14: closest beacon is at x=10, y=16\nSensor at x=10, y=20: closest beacon is at x=10, y=16\nSensor at x=14, y=17: closest beacon is at x=10, y=16\nSensor at x=8, y=7: closest beacon is at x=2, y=10\nSensor at x=2, y=0: closest beacon is at x=2, y=10\nSensor at x=0, y=11: closest beacon is at x=2, y=10\nSensor at x=20, y=14: closest beacon is at x=25, y=17\nSensor at x=17, y=20: closest beacon is at x=21, y=22\nSensor at x=16, y=7: closest beacon is at x=15, y=3\nSensor at x=14, y=3: closest beacon is at x=15, y=3\nSensor at x=20, y=1: closest beacon is at x=15, y=3"
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        items = String.split(line, " ", trim: true)
+
+        sx =
+          Enum.at(items, 2)
+          |> String.split("=")
+          |> Enum.at(1)
+          |> String.replace(",", "")
+          |> String.to_integer()
+
+        sy =
+          Enum.at(items, 3)
+          |> String.split("=")
+          |> Enum.at(1)
+          |> String.replace(":", "")
+          |> String.to_integer()
+
+        bx =
+          Enum.at(items, 8)
+          |> String.split("=")
+          |> Enum.at(1)
+          |> String.replace(",", "")
+          |> String.to_integer()
+
+        by =
+          Enum.at(items, 9)
+          |> String.split("=")
+          |> Enum.at(1)
+          |> String.replace(":", "")
+          |> String.to_integer()
+
+        [{sx, sy}, {bx, by}]
+      end)
+      |> Enum.into(%{}, fn [{sx, sy}, {bx, by}] -> {{sx, sy}, {bx, by}} end)
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
